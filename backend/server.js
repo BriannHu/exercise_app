50e1756ae77027f683128a29dfaff5646afb3d24
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-08-02 00:08:04 -0400

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,20 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-
 app.use(cors())
+app.use(express.json())
+
+const URI = process.env.ATLAS_URI
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
