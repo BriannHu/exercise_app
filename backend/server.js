6323ad5182673d574b22d0d1cad7e5f656587122
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-08-01 23:49:03 -0400

    Updated server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(cors())
