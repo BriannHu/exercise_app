224ad3a6da05454a93f87c9d2081a6e6464b456f
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-08-05 14:08:33 -0400

    Added additional features to create user component

--- frontend/src/components/create-user.component.js
+++ frontend/src/components/create-user.component.js
@@ -3,6 +3,10 @@ import axios from 'axios'
 
 export default function CreateUser() {
     const [username, setUsername] = useState('');
+    const [gender, setGender] = useState('');
+    const [age, setAge] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0);
 
     const checkStatus = (status) => {
         if (status === 200) {
@@ -14,9 +18,10 @@ export default function CreateUser() {
 
     const submit = (e) => {
         e.preventDefault();
-        axios.post('http://localhost:5000/users/add', {username})
+        const user = {username, age, gender, weight, height}
+        axios.post('http://localhost:5000/users/add', user)
         .then(res => checkStatus(res.status));
-        setUsername('')
+        window.location = '/create'
     }
 
     
@@ -25,19 +30,72 @@ export default function CreateUser() {
         <div>
             <h3>Create New User</h3>
             <form onSubmit={submit}>
-            <div className="form-group"> 
-                <label>Username: </label>
-                <input
-                    type="text"
-                    required
-                    className="form-control"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <div className="form-group">
-                <input type="submit" value="Create User" className="btn btn-primary" />
-            </div>
+                <div className="form-group"> 
+                    <label>Username: </label>
+                    <input
+                        type="text"
+                        required
+                        className="form-control"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Age:</label>
+                    <input
+                        type="number"
+                        required
+                        className="form-control"
+                        onChange={(e) => setAge(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <div>
+                        <label>Gender:</label>
+                    </div>
+                    <div className="form-check">
+                        <input
+                            type="radio"
+                            id="genderChoice1"
+                            name="gender"
+                            value="male"
+                            className="form-check-input"
+                            onChange={(e) => setGender(e.target.value)}
+                        />
+                        <label className="form-check-label" htmlFor="genderChoice1"> Male</label>
+                    </div>
+                    <div className="form-check">
+                        <input
+                            type="radio"
+                            id="genderChoice2"
+                            name="gender"
+                            value="female"
+                            className="form-check-input"
+                            onChange={(e) => setGender(e.target.value)}
+                        />
+                        <label className="form-check-label" htmlFor="genderChoice2"> Female</label>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label>Weight (kg):</label>
+                    <input
+                        type="number"
+                        required
+                        className="form-control"
+                        onChange={(e) => setWeight(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Height (cm):</label>
+                    <input
+                        type="number"
+                        required
+                        className="form-control"
+                        onChange={(e) => setHeight(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input type="submit" value="Create User" className="btn btn-primary" />
+                </div>
             </form>
         </div>
     )
