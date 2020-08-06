8a73793cbfc1e9b914bb2a49dfaec295ab65cf7e
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-08-05 22:29:40 -0400

    Added dropdowns in navbar

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 export default class NavigationBar extends Component {
     render() {
@@ -8,12 +9,39 @@ export default class NavigationBar extends Component {
             <Link to="/" className="navbar-brand">Fitness Spotter</Link>
             <div className="expand navbar-expand">
             <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-              <Link to="/" className="nav-link">View Exercises</Link>
+              <li className="navbar-item dropdown">
+                <a className="nav-link dropdown-toggle" href="/#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Add Exercise
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <Link to="/aerobic/add" className="dropdown-item">Aerobic</Link>
+                  <div className="dropdown-divider"></div>
+                  <Link to="/strength/add" className="dropdown-item">Strength</Link>
+                  <div className="dropdown-divider"></div>
+                  <Link to="/stretch/add" className="dropdown-item">Stretch</Link>
+                  <div className="dropdown-divider"></div>
+                  <Link to="/balance/add" className="dropdown-item">Balance</Link>
+                </div>
+              </li>
+              <li className="navbar-item dropdown">
+                <a className="nav-link dropdown-toggle" href="/#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  View Exercise
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <Link to="/aerobic/" className="dropdown-item">Aerobic</Link>
+                  <div className="dropdown-divider"></div>
+                  <Link to="/strength/" className="dropdown-item">Strength</Link>
+                  <div className="dropdown-divider"></div>
+                  <Link to="/stretch/" className="dropdown-item">Stretch</Link>
+                  <div className="dropdown-divider"></div>
+                  <Link to="/balance/" className="dropdown-item">Balance</Link>
+                </div>
               </li>
               <li className="navbar-item">
               <Link to="/create" className="nav-link">Add Exercise</Link>
               </li>
+              
+              
               {/*
               <li className="navbar-item">
               <Link to="/user" className="nav-link">Add User</Link>
